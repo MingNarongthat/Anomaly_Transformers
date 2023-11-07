3a41f3f74c035658e82483c848d4f0a3c0f333b0
Narongthat Thanyawet <mingkp97@gmail.com>
2023-11-07 11:00:07 +0900

    update sandbox

--- src/sandbox_playground.py
+++ src/sandbox_playground.py
@@ -3,6 +3,8 @@
 
 import torch
 import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt 
 
 print(torch.__version__)
 print(torch.cuda.is_available())
@@ -42,3 +44,29 @@ input = torch.Tensor([1,1])
 print(input)
 output = nn(x)
 print(output[0][0])
+
+z = np.arange(2).repeat(40) 
+r = np.random.normal(z+1,0.25) 
+t = np.random.uniform(0,np.pi,80)
+xx = r*np.cos(t)
+yy = r*np.sin(t)
+X = np.array([xx,yy]).T
+
+X = torch.Tensor(X)  
+z = torch.LongTensor(z)
+print(z)
+
+optimizer = torch.optim.Adam(nn.parameters(),lr=0.1)
+cross_entropy = torch.nn.CrossEntropyLoss()
+for i in range(100):
+    a = nn(X)
+    output = cross_entropy(a,z) # loss function between (prediction, GT)
+    output.backward() # backwatd propagation
+    optimizer.step()  # optimizer update wieght
+    optimizer.zero_grad() ## change gradient to zero, otherwise it will be a plus from the previous epoch
+    
+predict_z = nn(X).argmax(1)
+print(predict_z)
+plt.scatter(z,predict_z)
+plt.savefig('/opt/project/tmp/sandbox.jpg')
+# plt.show()
