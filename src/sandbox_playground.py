1be41f6fcce014fc54db0cf035361459bc4c18ee
Narongthat Thanyawet <mingkp97@gmail.com>
2023-11-07 09:57:33 +0900

    update sandbox

--- src/sandbox_playground.py
+++ src/sandbox_playground.py
@@ -8,3 +8,12 @@ print(torch.cuda.is_available())
 
 x = torch.rand(3 ,4, dtype= torch.float32)
 print(x)
+
+y = torch.rand(3, 4)
+print(y)
+
+print(x+y)
+print(x*y)
+print(x/y)
+print(y/x)
+print(torch.matmul(x, torch.transpose(y,0,1)))
