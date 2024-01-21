96ce8111a4014f2f73e7fb47c26bfab9a52105b1
Narongthat Thanyawet <mingkp97@gmail.com>
2024-01-21 13:07:19 +0900

    evaluate script updated

--- src/Evaluate_Caption.py
+++ src/Evaluate_Caption.py
@@ -3,25 +3,30 @@ from pycocoevalcap.meteor.meteor import Meteor
 from pycocoevalcap.rouge.rouge import Rouge
 from pycocoevalcap.cider.cider import Cider
 
-ref_caption = {
-    "image1_id" :["soil fall down from the mountain and the grass field is on the cliff"]}
-predicted_caption = {
-    "image1_id" :["soil fall down from the cliff and the tree is on the cliff"]}
+def compute_scores(ref_caption, predicted_caption):
+    # Scoring
+    scorers = [
+        (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+        (Meteor(),"METEOR"),
+        (Rouge(), "ROUGE_L"),
+        (Cider(), "CIDEr")
+    ]
+    for scorer, method in scorers:
+        print(f'Computing {method} score...')
+        score, scores = scorer.compute_score(ref_caption, predicted_caption)
+        if type(method) == list:
+            for m, s in zip(method, score):
+                print(f"{m}: {s}")
+        else:
+            print(f"{method}: {score}")
 
-# Scoring
-scorers = [
-    (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
-    (Meteor(),"METEOR"),
-    (Rouge(), "ROUGE_L"),
-    (Cider(), "CIDEr")
-]
-for scorer, method in scorers:
-    print(f'Computing {method} score...')
-    score, scores = scorer.compute_score(ref_caption, predicted_caption)
-    if type(method) == list:
-        for m, s in zip(method, score):
-            print(f"{m}: {s}")
-    else:
-        print(f"{method}: {score}")
+ref_caption = {
+    "image1_id" :["soil fall down from the mountain and the grass field is on the cliff"]
+}
+predicted_caption = {
+    "image1_id" :["soil fall down from the cliff and the tree is on the cliff"]
+}
+
+compute_scores(ref_caption, predicted_caption)
 
 
